97ca7b08244ed41e382c1d45d2ca080e0bae1e71
Amarantz <se0022@hotmail.com>
2017-11-17 23:33:30 +0000

    Added Documentor to my Docs Directory

--- tests/Storage/MysqlPluginTest.php
+++ tests/Storage/MysqlPluginTest.php
@@ -3,6 +3,12 @@ use PHPUnit\Framework\TestCase;
 class MysqlPluginTest extends TestCase
 {
 
+	protected $harness;
+
+	public function setUp() {
+		$harness = new \PDO("mysql:dbname=;host=localhost", 'appuser', 'Kmustang@1');
+	}
+
 	public function testCanaire()
 	{
 		$this->assertTrue(true);
